1e3e1ac231124466bd041d2cb39a70a7f26d1d66
Bhargav <vyasb1503@gmail.com>
2025-03-10 23:38:07 +0530

    this is the DTO of the user contain the email and password

--- src/main/java/com/tkaDTO/UserLoginDTO.java
+++ src/main/java/com/tkaDTO/UserLoginDTO.java
@@ -2,4 +2,38 @@ package com.tkaDTO;
 
 public class UserLoginDTO {
 
+	private String email;
+	private String Password; 
+	
+	public UserLoginDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserLoginDTO(String email, String password) {
+		super();
+		this.email = email;
+		Password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserLoginDTO [email=" + email + ", Password=" + Password + "]";
+	}
+	
 }
