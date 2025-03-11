d070d102e7bf0cfef5e907d71172144f0c1a4f57
Bhargav <vyasb1503@gmail.com>
2025-03-11 23:07:44 +0530

    this is the addition of the git dependency

--- src/main/java/com/tkaService/UserService.java
+++ src/main/java/com/tkaService/UserService.java
@@ -1,5 +1,13 @@
 package com.tkaService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tkaRepository.UserRepository;
+
 public class UserService {
+	 @Autowired
+	private UserRepository  userRepostory;
+	 
+	 
 
 }
