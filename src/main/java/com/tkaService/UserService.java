697ed8213b3cf021c6d4713c515e02526950eb3d
Bhargav <vyasb1503@gmail.com>
2025-03-12 23:09:36 +0530

    this is the remove of  the security dependency

--- src/main/java/com/tkaService/UserService.java
+++ src/main/java/com/tkaService/UserService.java
@@ -17,13 +17,18 @@ public class UserService {
 
 	public String registerUser(String userName, String password, String role) {
 	Optional<User> existingUser = userRepostory.findByName(userName);
+	System.out.println("1");
 		if(existingUser.isPresent()) {
+			System.out.println("2");
 			return "Error :User already exist";
+			
 		}
 		String encryptedPassword =passwordEncoder.encode(password);
-	
+		System.out.println("3");
 		User newUser = new User(userName, role, encryptedPassword);
+		System.out.println("4");
 		userRepostory.save(newUser);
+		System.out.println("5");
 		return "User registered successfully!";
 	}
 	
